5a39fcdf608139950f2dc57454f2b497c50ca4cf
David Gettins <dgetts192@gmail.com>
2015-11-17 21:12:01 +0000

    Initial setup.
    Assets folder, gitignore and gitattributes.

---
+++ Assets/NetworkSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(!isLocalPlayer){
+			GetComponent<AudioSource>().enabled = false;
+			transform.FindChild("Ethan").gameObject.SetActive(true);
+			transform.FindChild("FirstPersonCharacter").GetComponent<AudioListener>().enabled = false;
+			transform.FindChild("FirstPersonCharacter").GetComponent<Camera>().enabled = false;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
